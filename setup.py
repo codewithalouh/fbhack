fd34d9e8047d3eab24c2e3143f6c65b7b44e7cd0
Alouh Sperk <59327781+codewithalouh@users.noreply.github.com>
2022-12-20 14:00:13 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+import os
+
+try:
+  import colorama, requests 
+  os.system("python3 fbhack.py")
+  
+
+except:
+  os.system("pip3 install colorama")
+  os.system("pip3 install requests")
+  os.system("clear")
+  os.system("python3 fbhack.py")
+  
