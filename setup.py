1ed79f4ee6c88eaf474926601f8af9f6d1db1638
Alouh Sperk <59327781+codewithalouh@users.noreply.github.com>
2023-01-01 14:13:05 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,17 +2,21 @@
 import os
 
 try:
-  import colorama, requests, pycrypto
-  os.system("pkg install wget")
+  import colorama, hashlib, pycrypto, telegram
   os.system("clear")
   os.system("python3 fbhack.py")
   
 
 except:
   os.system("pip3 install colorama")
-  os.system("pip3 install requests")
-  os.system("pkg install wget")
+  os.system("pip3 install hashlib")
   os.system("pip3 install pycryptodome")
+  os.system("pip3 install python-telegram-bot")
   os.system("clear")
   os.system("python3 fbhack.py")
+  
+  
+
+
+
   
